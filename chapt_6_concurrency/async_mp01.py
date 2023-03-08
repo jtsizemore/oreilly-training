5417936240455c59476a75d1446249a11348dc84
jtsizemore <jtsizemore@outlook.com>
2023-03-08 16:04:03 +0000

    update

--- chapt_6_concurrency/async_mp01.py
+++ chapt_6_concurrency/async_mp01.py
@@ -6,3 +6,25 @@ systems.
 import os
 
 
+pid_list = []
+
+
+def main():
+    pid_list.append(os.getpid())
+    child_pid = os.fork()
+
+    if child_pid == 0:
+        pid_list.append(os.getpid())
+        print()
+        print("CHLD: hey, I am the child process")
+        print("CHLD: all the pids i know {0}".format(pid_list))
+
+    else:
+        pid_list.append(os.getpid())
+        print()
+        print("PRNT: hey, I am the parent ")
+        print("PRNT: the child pid is %d" % child_pid)
+        print("PRNT: all the pids I know %s" % pid_list)
+
+if __name__ == "__main__":
+    main()
